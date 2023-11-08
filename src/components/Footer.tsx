6cb16f9bb8d43a3fbb372b2942eee0e8dcc58ad7
petsamuel <petsamuel4@gmail.com>
2023-11-08 22:06:12 +0100

    initial commit

--- src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <div className="top-[20vh] relative  mt-6 flex justify-center text-center items-center w-max bottom-0 text-black">
-      <p className="  w-max text-center bottom-2">&copy; 2023</p>
-    </div>
-  );
-};
-
-export default Footer;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./src/**/*.{html,js,tsx}"],
+module.exports = {
+  content: ["**.html"],
   theme: {
     extend: {},
   },
